55e20abdfa2ecbc8a59bef35a4eb83722756bab1
Michael Brown <m.brown970514@gmail.com>
2023-01-06 22:48:39 +0800

    send size after finish file transfer

--- Transport/TransportServer.cpp
+++ Transport/TransportServer.cpp
@@ -158,9 +158,9 @@ DWORD WINAPI ProcessThread(LPVOID lpParam)
 				}
 
 				send(ConnectSocket,(char*)&dwSize,sizeof(dwSize),0);
+				char cRead[512];
 				do
 				{
-					char cRead[512];
 					DWORD nBytesRead = 0;
 					BOOL bResult = ReadFile(hFile,cRead,512,&nBytesRead,NULL); 
 					// Check for end of file. 
@@ -234,6 +234,7 @@ DWORD WINAPI ProcessThread(LPVOID lpParam)
 					//send(ConnectSocket, (char*)&dwWait, sizeof(dwWait), 0);
 					nBytesRecv += nBytesRead;
 				}
+				send(ConnectSocket, (char*)&nBytesRecv, sizeof(nBytesRecv), 0);
 				CloseHandle(hFile);
 			}
 			else if (sCommand.nCommand == CMD_CHK_DIR)//Check output directory.
@@ -391,7 +392,7 @@ int main( int argc, char **argv )
 
 		printf("One connection received\n");
 
-		// SetKeepAlive(sckConnected,TRUE);
+		//SetKeepAlive(sckConnected,TRUE);
 		ProcessSocket(sckConnected);
 	}
 
